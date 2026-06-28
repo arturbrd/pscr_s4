a15792712e24f11cf707c9a6f1405792c61a20df
arturbrd <artur.ab2002@gmail.com>
2026-06-28 17:17:47 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@ class MqttCallback : public virtual mqtt::callback {
         void message_arrived(mqtt::const_message_ptr msg) override {
             std::string msg_string = msg->to_string();
             std::string topic = msg->get_topic();
-            // std::cout << "Topic: " << msg->get_topic() << std::endl;
-            // std::cout << "Message: " << msg_string << std::endl;
+            std::cout << "Topic: " << msg->get_topic() << std::endl;
+            std::cout << "Message: " << msg_string << std::endl;
             if (topic == "station/c3/grid/raw") {
                 if (mq_send(mqtt_grid_queue, msg_string.c_str(), msg_string.size() + 1, 0) == -1) {
                     std::cerr << "Error: Couldn't send msg to queue" << std::endl;
