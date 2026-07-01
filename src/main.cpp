1e97e5c938f834dfed1101860d0963ae23ad708c
arturbrd <artur.ab2002@gmail.com>
2026-07-01 18:27:03 +0200

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -28,19 +28,19 @@ class MqttCallback : public virtual mqtt::callback {
                     std::cerr << "Error: Couldn't send msg to queue" << std::endl;
                     std::cerr << "mq_send errno: " << errno 
                             << " (" << strerror(errno) << ")" << std::endl;
-                } else std::cout << "Recceived from " << topic <, std::endl;
+                } else std::cout << "Recceived from " << topic << std::endl;
             } else if (topic == "weather/raw") {
                 if (mq_send(mqtt_weather_raw_queue, msg_string.c_str(), msg_string.size(), 0) == -1) {
                     std::cerr << "Error: Couldn't send msg to queue" << std::endl;
                     std::cerr << "mq_send errno: " << errno 
                             << " (" << strerror(errno) << ")" << std::endl;
-                } else std::cout << "Recceived from " << topic <, std::endl;
+                } else std::cout << "Recceived from " << topic << std::endl;
             } else if (topic == "weather/avg") {
                 if (mq_send(mqtt_weather_avg_queue, msg_string.c_str(), msg_string.size(), 0) == -1) {
                     std::cerr << "Error: Couldn't send msg to queue" << std::endl;
                     std::cerr << "mq_send errno: " << errno 
                             << " (" << strerror(errno) << ")" << std::endl;
-                } else std::cout << "Recceived from " << topic <, std::endl;
+                } else std::cout << "Recceived from " << topic << std::endl;
             }
         }
 };
